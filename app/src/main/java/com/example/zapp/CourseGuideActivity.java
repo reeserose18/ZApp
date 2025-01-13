fba2e9c2a3a1fc0ded029573653da985279936c2
reeserose18 <165043602+reeserose18@users.noreply.github.com>
2025-01-13 14:05:59 -0600

    HomeScreen&img

---
+++ app/src/main/java/com/example/zapp/CourseGuideActivity.java
@@ -0,0 +1,14 @@
+package com.example.zapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CourseGuideActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_guide);
+    }
+}
+
+
--- app/src/main/java/com/example/zapp/MainActivity.java
+++ app/src/main/java/com/example/zapp/MainActivity.java
@@ -1,38 +1,26 @@
 package com.example.zapp;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import android.widget.TextView;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import android.view.ViewGroup;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button syllabusButton = findViewById(R.id.btnSyllabus);
+        syllabusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CourseGuideActivity.class);
+                startActivity(intent);
+            }
         });
-
-            TextView textView = new TextView(this);
-            textView.setText("zamora App");
-            textView.setTextSize(24);
-            textView.setX(400);
-            textView.setY(500);
-
-            ViewGroup rootLayout = findViewById(android.R.id.content);
-            rootLayout.addView(textView);
-
-
     }
 }
+
+
